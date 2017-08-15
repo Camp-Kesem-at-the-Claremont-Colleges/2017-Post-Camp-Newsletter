3fc70ae2cad8135904441220f8842dda97528759
kcunanan <kevin.a.cunanan@gmail.com>
2017-08-15 15:12:48 -0700

    added quotes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,7 @@
 // Quote Randomization
 var quotes = [];
 var authors = [];
+var positions = [];
 {% assign i = 0 %}
 {% for quote in site.data.quotes %}
 quotes[{{ i }}] = {{ quote.quote }};
@@ -11,11 +12,12 @@ quotes[{{ i }}] = {{ quote.quote }};
     {% else %}
         authors[{{ i }}] = "";
     {% endif %}
+positions[{{ i }}] = "{{ quote.position }}"
 {% assign i = i | plus: 1 %}
 {% endfor %}
 var totalQ = {{ i }};
 var randomQ = Math.floor(Math.random() * totalQ);
-var loadQuote = $('<h2 id="quote-load">"' + quotes[randomQ] + '"</h2><h2>' + authors[randomQ] + '</h2><img class="loading-icon animate-flicker" src="/assets/img/sun.png">').hide().fadeIn(1500);
+var loadQuote = $('<h2 id="quote-load">"' + quotes[randomQ] + '"</h2><h2>' + authors[randomQ] + ' (' + positions[randomQ] + ') ' + '</h2><img class="loading-icon animate-flicker" src="/assets/img/sun.png">').hide().fadeIn(1500);
 $('#quote').append(loadQuote);
 
 // preloading
